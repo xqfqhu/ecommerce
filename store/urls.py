3e4bfa642c9ea8bab7a87e3427562f55883db76c
Yi Hu <yi_hu@alumni.brown.edu>
2021-07-22 00:47:05 +0800

    add search function

--- store/urls.py
+++ store/urls.py
@@ -4,7 +4,9 @@ from . import views
 
 app_name = 'store'
 urlpatterns = [
-    path('', views.all_products, name = 'all_products'),
+    path('', views.all_products, name = 'store_home'),
     path('item/<slug:slug>/', views.product_detail, name = 'product_detail'),
-    path('search/<slug:category_slug>/', views.category_list, name = 'category_list'),
+    
+    path("search", views.conduct_search, name="search"),
+    
 ]
