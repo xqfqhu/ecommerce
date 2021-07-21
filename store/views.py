dc64280e3d0fe156bd461da3b1836e29d201f077
Yi Hu <yi_hu@alumni.brown.edu>
2021-07-22 00:45:59 +0800

    add search function

--- store/views.py
+++ store/views.py
@@ -1,22 +1,64 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
+from .forms import searchForm
 # Create your views here.
 
 
 def all_products(request):
-    products = Product.objects.all()
-    return render(request, 'store/home.html', {'products': products})
-    
+    products = Product.products.all()
+    return render(request, 'store/index.html', {'products': products})
+
+
 def categories(request):
+
     return {
         'categories':Category.objects.all()
     }
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug = slug, in_stock = True)
-    return render(request, 'store/products/detail.html', {'product': product})
-def category_list(request, category_slug):
-    category = get_object_or_404(Category, slug = category_slug)
-    products = Product.objects.filter(category = category)
-    return render(request, 'store/products/category.html', {'products': products, 'category': category})
 
+    return render(request, 'store/single.html', {'product': product})
+
+
+def search(request):
+    
+    
+        
+    search_form = searchForm()
+        
+    return {'search_form':search_form}
+def conduct_search(request):
+    
+        
+
+    category_slug = request.GET.get('category')
+    if "keyword" in request.GET:
+        keyword = request.GET.get('keyword')
+    else:
+        keyword = ""
+
+
+    if category_slug != "all":
+        category = get_object_or_404(Category, slug = category_slug)
+                
+        category_recurse = category.recurse_children()
+        products = Product.products.filter(
+            category__in=category_recurse) & \
+                (Product.products.filter(title__contains=keyword) | \
+                    Product.products.filter(author__contains=keyword) | \
+                        Product.products.filter(compiler__contains = keyword) | \
+                            Product.products.filter(description__contains=keyword))
+
+        response = render(request, 'store/category.html', {'products': products, 'category': category,  'keyword': keyword})
+        return response
+                
+    else:
+        products = Product.products.filter(title__contains=keyword) | \
+                    Product.products.filter(author__contains=keyword) | \
+                        Product.products.filter(compiler__contains = keyword) | \
+                            Product.products.filter(description__contains=keyword)
+        return render(request, 'store/category.html', {'products': products,  'keyword': keyword})
+                
+            
+    
